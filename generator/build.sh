a87752a6ae9041bcd043daa463043326e0ecf91d
wangyonghua <wyh@2016>
2017-11-16 15:43:25 +0800

    add generator

---
+++ generator/build.sh
@@ -0,0 +1,1 @@
+java -jar /Users/liqiu/git/study/web/soft/mybatis-generator-core-1.3.2/lib/mybatis-generator-core-1.3.2.jar -configfile /Users/liqiu/git/study/web/soft/mybatisGeneratorConfig.xml -overwrite
